5527f0f317966fdc4d79d358fa3e4c88035f6d3f
Ant Geo <ant.geo@protonmail.com>
2022-06-08 00:06:26 +0300

    Adapted waves count.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 let shaderTexture;
 let shaders = [];
 let waves = [];
-let total = 50;
-let incoFont;
+let total = 32;
+let roboFont;
 
 let hw, qw, hh;
 let fps = 0;
 
 function preload() {
-  incoFont = loadFont('fonts/Roboto-Regular.ttf');
+  roboFont = loadFont('fonts/Roboto-Regular.ttf');
   sineShader = loadShader('shaders/waves.vert', 'shaders/sine.frag');
   addSynthShader = loadShader('shaders/waves.vert', 'shaders/addSynth.frag');
   ampModShader = loadShader('shaders/waves.vert', 'shaders/ampMod.frag');
@@ -16,7 +16,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(displayWidth, displayHeight, WEBGL);
+  setAttributes('antialias', true);
+  setAttributes('depth', false);
+
   // blendMode(SCREEN)
   
   hw = width * 0.5;
@@ -30,92 +33,90 @@ function setup() {
 
   shaders = [sineShader, addSynthShader, ampModShader, freqModShader];
   
-  textFont(incoFont, 32);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  textFont(roboFont, 32);
 }
 
 function draw() {
-  noStroke();
+  noStroke()
   
   for (const sh of shaders) {
-    sh.setUniform('u_time', frameCount * 0.2);
-    sh.setUniform('u_resolution', [600, 400]);    
+    sh.setUniform('u_time', frameCount * 0.3)
+    sh.setUniform('u_resolution', [600, 400])
   }
 
   background(0);
   texture(shaderTexture);
   
-  let remove = [];
+  let remove = []
   for (const w of waves) {
-    w.draw();
-    w.advance();
-    let offset = 2 * w.width;
+    w.draw()
+    w.advance()
+    let offset = 2 * w.width
     if (w.entered) {
       if (w.pos.x > hw + offset || w.pos.y > hh + offset || w.pos.x < -hw - offset || w.pos.y < -hh - offset) {
-        remove.push(waves.indexOf(w));
+        remove.push(waves.indexOf(w))
       }      
     } else {
       if (w.pos.x < hw + offset && w.pos.y < hh + offset && w.pos.x > -hw - offset && w.pos.y > -hh - offset) {
-        w.entered = true;
+        w.entered = true
       }
     }
   }
   
   for (let i = remove.length - 1; i >= 0; i--) {
-    waves.splice(remove[i], 1);
+    waves.splice(remove[i], 1)
   }
   
-  let i = total - waves.length;
+  let i = total - waves.length
   while(i-- > 0) {
-    waves.push(new Wave());
+    waves.push(new Wave())
   }
   
-  if (frameCount % 10 == 0) {
-    fps = frameRate();
-    if (fps > 61) {
-      total += 6;
-    } else if (total > 12 && fps < 50) {
-      total -= 6;
+  if (frameCount % 10 == 0)
+    fps = frameRate()
+  
+  if (frameCount % 30 == 0) {
+    if (fps > 59) {
+      total += 6
+    } else if (total > 12 && fps < 58) {
+      total -= 6
     }
   }
   
-  fill(225);
-  // strokeWeight(6)
-  stroke(10);
-  text(nf(fps, 3, 1), -hw + 10, -hh + 30);
-  text(total, -hw + 10, -hh + 60);
+  fill(225)
+  stroke(10)
+  text(nf(fps, 3, 1), -hw + 10, -hh + 30)
+  text(total, -hw + 10, -hh + 60)
+  text(waves.length, -hw + 10, -hh + 90)
 }
 
 class Wave {
 
   constructor() {
-    this.shader = shaders[floor(random(0, shaders.length))];
-    this.freq = random(24.0, 36.0);
-    this.harm = pow(2, floor(random(0, 3)));
-    this.width = random(120, 220);
-    this.height = random(120, 220);
-    this.entered = false;
-    this.speed = random(3, 6);
-    this.orientation = createVector(random(-1, 1), random(-1, 1)).normalize();
+    this.shader = shaders[floor(random(0, shaders.length))]
+    this.freq = random(24.0, 36.0)
+    this.harm = pow(2, floor(random(0, 3)))
+    this.width = random(120, 220)
+    this.height = random(120, 220)
+    this.entered = false
+    this.speed = random(3, 6)
+    this.orientation = createVector(random(-1, 1), random(-1, 1)).normalize()
     
     let offset = this.height //incorrect, but works and it's fast
     if (this.orientation.x < 0 && this.orientation.y < 0) {
-      this.pos = createVector(random(-qw, qw), hh + offset);
+      this.pos = createVector(random(-qw, qw), hh + offset)
     } else if (this.orientation.x < 0 && this.orientation.y > 0) {
-      this.pos = createVector(random(-qw, qw), -hh - offset);
+      this.pos = createVector(random(-qw, qw), -hh - offset)
     } else if (this.orientation.x > 0 && this.orientation.y > 0) {
-      this.pos = createVector(random(-qw, qw), -hh - offset);
+      this.pos = createVector(random(-qw, qw), -hh - offset)
     } else {
-      this.pos = createVector(random(-qw, qw), hh + offset);
+      this.pos = createVector(random(-qw, qw), hh + offset)
     }
-    this.pos.sub(p5.Vector.mult(this.orientation, this.width));
+    this.pos.sub(p5.Vector.mult(this.orientation, this.width))
   }
   
   advance() {
-    this.pos.add(p5.Vector.mult(this.orientation, this.speed));
+    this.pos.add(p5.Vector.mult(this.orientation, this.speed))
   }
   
   draw() {
@@ -124,10 +125,10 @@ class Wave {
     shaderTexture.shader(this.shader);
     shaderTexture.rect(0,0,10,10);
 
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.orientation.heading());
-    rect(0, 0, this.width, this.height);
-    pop();
+    push()
+    translate(this.pos.x, this.pos.y)
+    rotate(this.orientation.heading())
+    rect(0, 0, this.width, this.height)
+    pop()
   }
 }
